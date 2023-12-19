02bedf1440697994a6c9db0ce462d98c0b3f4030
OscarOrtiz <tippingtheiceberg@gmail.com>
2023-12-19 01:03:45 -0600

    add playerTurn function, add ability to get data attirbute needed for adding player symbol

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 2. The plan will be to create a game predominately built off of objects and JS factories. 
 3. Limit the use of global variables
 4. Fun to make it suddenly very intense once players are selected
-5.
+5. When users are selected, the ability to change names should not be available
+6. When users are not selected, the ability to place X's or O's should not be available
+7. If users decide to change names, the grid should reset.
+8. To win, 3 of the same shapes should be in a row.
 
 Music On user entry screen
 A Kind Of Hope by Scott Buckley | www.scottbuckley.com.au
--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let TicTacToe = {
-    grid: [],
+    playerTurn: 0,
     init: function(){
         this.domCache();
         this.bindEvents();
-        this.addPlayer1();
+        this.checkDiag();
+        this.checkRows();
+        this.checkColumns();
+        this.getDataAttribute();
+        this.decidePlayerTurn();
+        this.player1Turn();
+        this.player2Turn();
+        this.placePlayerSymbol();
     },
     domCache: function(){
         this.el = document.querySelector(".playingField");
@@ -15,6 +22,20 @@ let TicTacToe = {
         this.player2Val = document.querySelector("#player2");
         this.player2Btn = this.el.querySelectorAll(".player2__btn");
         this.player2 = this.el.querySelectorAll(".player2__playerName");
+
+        this.getAllSpaces = this.el.querySelectorAll('.grid')
+
+        this.row1 = this.el.querySelectorAll(".row1");
+        this.row2 = this.el.querySelectorAll(".row2");
+        this.row3 = this.el.querySelectorAll(".row3");
+
+        this.column1 = this.el.querySelectorAll(".column1");
+        this.column2 = this.el.querySelectorAll(".column2");
+        this.column3 = this.el.querySelectorAll(".column3");
+
+        this.diag1 = this.el.querySelectorAll(".diag1");
+        this.diag2 = this.el.querySelectorAll(".diag2");
+        
     } ,
     bindEvents: function(){
         this.player1Btn.forEach(btn => {
@@ -23,24 +44,61 @@ let TicTacToe = {
         this.player2Btn.forEach(btn => {
             btn.addEventListener("click",this.addPlayer2.bind(this));
         })
+
+        this.getAllSpaces.forEach(space=>{
+            space.addEventListener("click", () => this.getDataAttribute(space.getAttribute("data-attribute")))
+        })
         
     },
 
     addPlayer1: function(){
         this.player1.forEach(a => a.textContent = this.player1Val.value)
-        this.inti
         this.player1Val.value = ""
     },
     addPlayer2: function(){
         this.player2.forEach(b => b.textContent = this.player2Val.value)
-        this.init
         this.player2Val.value = ""
+    },
+    // da = data-attribute
+    getDataAttribute:function(da){
+        this.decidePlayerTurn(da)
+    },
+    decidePlayerTurn:function(da){
+        if(this.playerTurn % 2 === 0){
+            return this.player1Turn(da,this.playerTurn)
+        }
+        else{
+            console.log(this.playerTurn);
+            return this.player2Turn(da, this.playerTurn)
+        }
+    },
+    player1Turn: function(da,playerTurn){
+        this.playerTurn = 1
+        this.placePlayerSymbol(da,playerTurn)
+        
+    },
+    player2Turn:function(da, playerTurn){
+        this.playerTurn = 2
+        this.placePlayerSymbol(da,playerTurn)
+    },
+    placePlayerSymbol: function(da,playerTurn){
+        console.log(da,playerTurn);
+    },
+    checkRows: function(){
+        this.row1.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
+        this.row2.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
+        this.row3.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
+    },
+    checkColumns: function(){
+        this.column1.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
+        this.column2.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
+        this.column3.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
+
+    },
+    checkDiag: function(){
+        this.diag1.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
+        this.diag2.forEach(spot => console.log(spot.children[0].getAttribute('data-symbol')));
     }
-
-    
-
-
-
 
 }
 
